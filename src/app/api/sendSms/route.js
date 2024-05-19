8b0e658af44f6d27710e04c8598a9edc6ec4e57d
abhaysharma2719 <whoabhaysharma@gmail.com>
2024-05-19 10:57:34 +0530

    added the notification keys to .env

--- src/app/api/sendSms/route.js
+++ src/app/api/sendSms/route.js
@@ -1,29 +1,34 @@
 import notificationapi from 'notificationapi-node-server-sdk'
 // const notificationapi = require('notificationapi-node-server-sdk').default
 
-notificationapi.init(
-    '4066qkpm1hfchjj24go590k59k', // clientId
-    'sdo0696u5to4a48dph13k25gsf66grnec6tlvb8b8n8gtfr6alj'// clientSecret
-)
+const clientId = process.env.NEXT_PUBLIC_NOTIFICATION_CLIENTID
+const clientSecret = process.env.NEXT_PUBLIC_NOTIFICATION_CLIENT_SECRET
 
+notificationapi.init(
+    clientId, // clientId
+    clientSecret// clientSecret
+)
 
 export async function GET(req) {
     const { searchParams } = new URL(req.url)
     const phone = searchParams.get('phone')
 
     if (!phone) {
-        return Response.json({"message" : "please enter phone and message both"})
+        return Response.json({ "message": "please enter phone and message both" })
     }
 
-
-    notificationapi.send({
-        notificationId: 'test_notification',
-        user: {
-            id: "abhaysharma.as2719@gmail.com",
-            number: "+91" + phone // Replace with your phone number
-        },
-        mergeTags: {}
-    })
+    try {
+        notificationapi.send({
+            notificationId: 'test_notification',
+            user: {
+                id: "abhaysharma.as2719@gmail.com",
+                number: "+91" + phone // Replace with your phone number
+            },
+            mergeTags: {}
+        })
+    } catch (e) {
+        console.log("-------------------caught with an error --------------------", e)
+    }
 
     return Response.json({"message" : "sent successfullly"})
 }
